d6cef9ce65c5c9d7dce07d361cc6290b709b1fb0
Mabuza98 <70321095+Mabuza98@users.noreply.github.com>
2020-11-19 12:00:53 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+using System.Collections.Generic;
+
+
+namespace mps_assignment_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string charCounts = Words.CountCharactersInSentence("My name is justice mabuza");
+
+            Console.WriteLine(charCounts);
+
+        }
+    }
+}
---
+++ Words.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public static class Words
+
+{
+    public static string CountCharactersInSentence(string inputString)
+
+    {
+        Dictionary<char, int> Newstring = new Dictionary<char, int>();
+        string count = inputString;
+        foreach (char characters in count.Replace(" ", string.Empty))
+        {
+            if (Newstring.ContainsKey(characters))
+            {
+                Newstring[characters] = Newstring[characters] + 1;
+            }
+            else
+            {
+                Newstring.Add(characters, 1);
+            }
+        }
+        foreach (var item in Newstring.Keys)
+        {
+            count += " " + item + " -> " + Newstring[item] + " ";
+        }
+        return count;
+
+    }
+}
